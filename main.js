c42c31b06c67e82a0b913a8e45e9009b554df397
Noah Laferty <noah.lafferty@gmail.com>
2015-09-10 17:02:38 -0500

    game complete!

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 var quotes = [
-	{ quote: "Everyone's got friends when they’re 21.", origin: "jodj" },
+	{ quote: "Everyone's got friends when they're 21.", origin: "jodj" },
 	{ quote: "Who needs running water when we can go to a fountain?", origin: "jodj" },
 	{ quote: "Having a beer would be too much work. Instead, I'll just have a strong rum drink.", origin: "jodj" },
-	{ quote: "I just do what satisfies me… it’s all temporary.", origin: "jodj" },
-	{ quote: "You know what we should do. We should take one of these couches and put wheels on it and turn it into a car.", origin: "jodj" },
+	{ quote: "I just do what satisfies me... it's all temporary.", origin: "jodj" },
+	{ quote: "You know what we should do? We should take one of these couches and put wheels on it and turn it into a car.", origin: "jodj" },
 	{ quote: "There's no such thing as too turnt.", origin: "jodj" },
-	{ quote: "Nobody washes the dishes! We eat the food directly off the coffee table and you know it.", origin: "frank" },
+	{ quote: "Nobody washes the dishes! We eat food directly off the coffee table and you know it.", origin: "frank" },
 	{ quote: "Two dudes getting married, that doesn't seem very gay.", origin: "frank" },
 	{ quote: "I don't know how many years on this earth I got left. I'm gonna get real weird with it.", origin: "frank" },
 	{ quote: "We could be in a turtle's dream in outer space", origin: "frank" }
 ];
+
+var current = 0;
+var score = 0;
 
 function shuffle(o){
     for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
@@ -18,7 +21,66 @@ function shuffle(o){
 
 function init() {
 	shuffle(quotes);
-	console.log(quotes);
+
+	$('.quote span').text(quotes[current].quote);
+	$('section.start').fadeOut(400, function() {
+		$('section.game').fadeIn(400);
+	});
 }
 
-init();
+function nextQuote() {
+	current++;
+
+	$('.choice').removeClass('right wrong');
+	$('.choice').removeAttr('disabled');
+
+	if (current < quotes.length) {
+		$('.quote').fadeOut(400, function() {
+			$('.quote span').text(quotes[current].quote);
+			$('.quote').fadeIn(400);
+		});
+		$('.next').attr('disabled', 'disabled');
+	} else {
+		endGame();
+	}
+}
+
+function endGame() {
+	$('.score').text((score / quotes.length) * 100);
+	$('section.game').fadeOut(400, function() {
+		$('section.results').fadeIn(400);
+	});
+}
+
+function replay() {
+	current = 0;
+	score = 0;
+
+	$('section.results').fadeOut(400, function() {
+		$('section.start').fadeIn(400);
+	});
+}
+
+$('.choice').click(function() {
+	var choice = $(this).attr('data-choice');
+
+	$('.choice').attr('disabled', 'disabled');
+
+	if (choice == quotes[current].origin) {
+		$(this).addClass('right');
+		score++;
+	} else {
+		$('.choice').addClass('right');
+		$(this).removeClass('right').addClass('wrong');
+	}
+
+	setTimeout(function() {
+		$('.next').removeAttr('disabled');
+	}, 600);
+});
+
+$('section.game, section.results').css({ display: 'none' });
+
+$('.start').click(init);
+$('.next').click(nextQuote);
+$('.replay').click(replay);
